87e59450e33972d23db323a1ed7a055d8d11e61e
gonzalo <gibarsin@itba.edu.ar>
2016-10-21 20:52:12 -0300

    src and sample with scanner

---
+++ src/Sample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by gonzalo on 10/21/16.
+ */
+public class Sample {
+  public static void main(String[] args) {
+    final Scanner scanner = new Scanner(System.in);
+    final int testCases = scanner.nextInt();
+
+    for(int i=0; i < testCases; i++) {
+      //Insert code here
+    }
+  }
+}
